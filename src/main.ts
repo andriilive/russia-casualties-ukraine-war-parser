0e09ea8a83509800821f61b52c95b50a21ec2778
Andrii Iv. (@digitalandyeu) <hi@digitalandy.eu>
2022-11-20 01:24:37 +0100

    working crawler

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
-// For more information, see https://crawlee.dev/
-import { PuppeteerCrawler, log } from 'crawlee';
-import { router } from './routes';
+import { PuppeteerCrawler, Dataset } from 'crawlee';
 
-const startUrls = ['https://crawlee.dev'];
-
+// PuppeteerCrawler crawls the web using a headless
+// browser controlled by the Puppeteer library.
 const crawler = new PuppeteerCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
+    // Use the requestHandler to process each of the crawled pages.
+    async requestHandler({ request, page, enqueueLinks, log }) {
+        const title = await page.title();
+        log.info(`Title of ${request.loadedUrl} is '${title}'`);
+
+        // Save results as JSON to ./storage/datasets/default
+        await Dataset.pushData({ title, url: request.loadedUrl });
+
+        // Extract links from the current page
+        // and add them to the crawling queue.
+        await enqueueLinks();
+    },
+    // Uncomment this option to see the browser window.
+    // headless: false,
 });
 
-await crawler.run(startUrls);
+// Add first URL to the queue and start the crawl.
+await crawler.run(['https://crawlee.dev']);
