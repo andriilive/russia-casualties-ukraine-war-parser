2f96a82262adba8555ab957ec1d07dc000a08dd3
Andrii Iv. (digitalandyeu) <hi@digitalandy.eu>
2023-10-24 07:47:29 +0200

    Better docs

--- scrap.js
+++ scrap.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const html = fs.readFileSync('.snapshot.html', 'utf8')
 const cheerio = require('cheerio')
-const {getDaysIds, router, db, warDayNumber, getTheDay, putToday} = require("./utils");
+const {getDaysIds, router, db, warDayNumber, getTheDay, putToday, casualtiesKeys} = require("./utils");
 const $ = cheerio.load(html);
 
 const casualties = $('.war_num').get().map((el) => {
@@ -11,20 +11,14 @@ const casualties = $('.war_num').get().map((el) => {
 let parsedDay = $('.war_title').text().match(/\d+/)[0];
 
 let parsedResult = {
-    id: Number(parsedDay),
-    militaryPersonnel: casualties[0],
-    jet: casualties[1],
-    copter: casualties[2],
-    tank: casualties[3],
-    armoredCombatVehicle: casualties[4],
-    artillerySystem: casualties[5],
-    airDefenceSystem: casualties[6],
-    mlrs: casualties[7],
-    supplyVehicle: casualties[8],
-    ship: casualties[9],
-    uav: casualties[10],
-    created_at: new Date().toISOString()
+    id: Number(parsedDay)
 }
+
+casualtiesKeys.forEach((key, index) => {
+    parsedResult[key] = casualties[index];
+});
+
+parsedResult['created_at'] = new Date().toISOString();
 
 console.log('scrap.js result: ', parsedResult);
 
--- utils.js
+++ utils.js
@@ -2,6 +2,8 @@ const jsonServer = require("json-server");
 const warStartDate = new Date('2022-02-24').getTime()
 const warDayNumber = Number(Math.floor((Date.now() - warStartDate) / 86400000) + 1);
 const router = jsonServer.router('db.json')
+const packageJson = require('./package.json')
+const {homepage} = packageJson
 
 const getTheDay = async (id) => {
     const day = await router.db.get('days').find({id}).value()
@@ -26,13 +28,14 @@ const putToday = async function (dataRaw) {
     return result
 }
 
-const publicUrl = process.env.VERCEL_URL ?? `http://localhost:${process.env.PORT ?? 3000}`
+const publicUrl = 'https://' + process.env.VERCEL_URL ?? `http://localhost:${process.env.PORT ?? 3000}`
 const staticApiUri = (url) => `${process.env.VERCEL_URL ?? 'http://localhost:3000'}/json-api/${url}.json`;
 const publicApiUri = (url) => `${process.env.VERCEL_URL ?? 'http://localhost:3000'}/api/${url}`;
 
 const db = router.db;
 
+const casualtiesKeys = ["militaryPersonnel", "jet", "copter", "tank", "armoredCombatVehicle", "artillerySystem", "airDefenceSystem", "mlrs", "supplyVehicle", "ship", "uav"];
 
 module.exports = {
-    warStartDate, warDayNumber, getDaysIds, getLastDay, getTheDay,putToday, publicUrl, db, staticApiUri,publicApiUri
+    warStartDate, warDayNumber, getDaysIds, getLastDay, getTheDay,putToday, publicUrl, db, staticApiUri, casualtiesKeys, publicApiUri, homepage
 }
