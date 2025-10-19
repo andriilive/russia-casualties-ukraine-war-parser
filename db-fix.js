bed9b31b80ca1a5505712c74c0fcfa6e59dcc79b
Andrii Iv. (@digitalandyeu) <43708848+andriilive@users.noreply.github.com>
2025-10-19 11:31:31 +0200

    updated db_fix

--- db-fix.js
+++ db-fix.js
@@ -8,64 +8,84 @@ async function fetchDay(dayId) {
   // format date to yyyy-mm-dd
   let date = fullDate.toISOString().split('T')[0];
   const fetchUrl = `https://russianwarship.rip/api/v2/statistics/${date}`;
-  let dayData;
+  let dayData = null;
 
   try {
-    const res = await fetch(fetchUrl);
-    const {stats} = (await res.json()).data;
-    dayData = {
-      id: dayId,
-      "militaryPersonnel": stats['personnel_units'],
-      "jet": stats['planes'],
-      "copter": stats['helicopters'],
-      "tank": stats['tanks'],
-      "armoredCombatVehicle": stats['armoured_fighting_vehicles'],
-      "artillerySystem": stats['artillery_systems'],
-      "airDefenceSystem": stats['aa_warfare_systems'],
-      "mlrs": stats['mlrs'],
-      "supplyVehicle": stats['vehicles_fuel_tanks'],
-      "ship": stats['warships_cutters'],
-      "uav": stats['uav_systems'],
-      // "date": date, // 2023-12-26
-      "created_at": new Date(date).toISOString(),
-    };
-  } catch (error) {
-    console.error(`Error fetching day #${dayId} at ${fetchUrl}:`, error);
-    process.exit(1);
-    throw error;
+    const fetchDay = await fetch(fetchUrl);
+    const res = await fetchDay.json();
+    console.log(res);
+    if (res && res) {
+      const stats = res.data.stats;
+      dayData = {
+        id: dayId,
+        "militaryPersonnel": stats['personnel_units'],
+        "jet": stats['planes'],
+        "copter": stats['helicopters'],
+        "tank": stats['tanks'],
+        "armoredCombatVehicle": stats['armoured_fighting_vehicles'],
+        "artillerySystem": stats['artillery_systems'],
+        "airDefenceSystem": stats['aa_warfare_systems'],
+        "mlrs": stats['mlrs'],
+        "supplyVehicle": stats['vehicles_fuel_tanks'],
+        "ship": stats['warships_cutters'],
+        "uav": stats['uav_systems'],
+        // "date": date, // 2023-12-26
+        "created_at": new Date(date).toISOString(),
+      }
+    }
+    else {
+      console.error('No stats found for day #' + dayId + ' (' + date + ')');
+      dayData = null;
+      return null;
+    }
+  }
+  catch (error) {
+    console.error('Error fetching data for day #' + dayId + ' (' + date + '): ', error);
+    dayData = null;
+    return null;
   }
   return dayData;
 }
 
 (async function main() {
   try {
+    const firstDayId = db.get('days').first().value().id;
     const lastDayId = db.get('days').last().value().id;
     const currentDayId = warDayNumber;
 
-    if (lastDayId >= currentDayId) {
-      console.log('No gaps to fill. Last day ID in DB:', lastDayId, 'Current day ID:', currentDayId);
-      process.exit(0);
-      return;
-    }
+    let dayId = lastDayId - 1;
+    while (dayId > firstDayId) {
 
-    let dayI = lastDayId + 1;
-    while (dayI <= currentDayId) {
-      const dayExists = db.get('days').find({id: dayI}).value();
-      if (dayExists) {
-        console.log(`fill-gaps.js: day #${dayI} exists in db.json`);
-        dayI++;
-        continue;
-      }
+      const dayExists = db.get('days').find({id: dayId}).value();
+      if (!dayExists) {
 
-      const dayData = await fetchDay(dayI);
-      if (dayData) {
-        await db.get('days').push(dayData).write();
-        console.log(`fill-gaps.js: added day #${dayI} to db.json`);
+        console.warn('Day #' + dayId + ' is missing. Fetching and adding to the database...');
+
+        if (dayId === 3) {
+          console.log('Skipping day #3 due to known data issues.');
+          dayId--;
+          continue;
+        }
+
+        try {
+          const dayData = await fetchDay(dayId);
+          if (dayData) {
+            db.get('days').push(dayData).write();
+            console.log('Day #' + dayI + ' added successfully.');
+          }
+          else {
+            console.error('Failed to fetch data for day #' + dayI + '.');
+          }
+        }
+        catch (error) {
+          console.error('Error processing day #' + dayI + ': ', error);
+        }
+
       }
-      dayI++;
+      dayId--;
     }
-    if (dayI === currentDayId) {
-      console.log('All gaps filled from day #'+(lastDayId + 1)+' to today #'+currentDayId);
+    if (dayId === currentDayId) {
+      console.log('All gaps filled from day #' + (lastDayId + 1) + ' to today #' + currentDayId);
     }
   }
   catch (error) {
